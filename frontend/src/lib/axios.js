f08af0fb905c09ae703afedbc93dc0a396a1731d
Srihari Saiyapu <saiyapusrihari2006@gmail.com>
2026-06-29 00:12:30 +0530

    connect frontend to render backend

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: '/api',
+  baseURL: "https://e-commers-website-x2y2.onrender.com",
   headers: {
     'Content-Type': 'application/json',
   },
